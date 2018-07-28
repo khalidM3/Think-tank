f1b2fa85aef7e0590da615fe5b93d44d5ec2a2fd
Khalid Mohamud <kmohamud1@outlook.com>
2018-07-27 23:41:31 -0700

    make fake api

---
+++ src/config/data/problem.js
@@ -0,0 +1,42 @@
+import faker from 'faker'
+import User from './user'
+import {looper, promisify} from './util'
+
+class Problem {
+  constructor(owner) {
+    this.id = faker.random.uuid()
+    this.name = faker.name.title()
+    this.desc = faker.lorem.paragraph()
+    this.followers = []
+    this.subProblems = []
+  }
+}
+
+
+Problem.fetchFakeProblems = (num = 1, cb) => {
+  try {
+    let arr = []
+    looper(num)( () => {
+      return arr.push(new Problem())
+    })
+    return cb(null, arr) 
+  } catch(err) {
+    return cb(err, null) 
+  }
+}
+
+Problem.fetchFakeUserProblems = (num = 1, owner, cb) => {
+  try {
+    let arr = []
+    looper(num)( () => {
+      return arr.push(new Problem(owner))
+    })
+    return  cb(null, arr) 
+  } catch(err) {
+      return cb(err, null)
+  }
+}
+
+export default Problem
+
+
