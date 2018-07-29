e256818cabbd70bc28917f1ec6be89388422c76e
Khalid Mohamud <kmohamud1@outlook.com>
2018-07-28 21:27:30 -0700

    added structure to app

--- src/config/data/util.js
+++
@@ -1,9 +0,0 @@
-export const looper = (num) => (cb) => {
-  for(var i=0; i <num; i++) {
-    cb()
-  }
-}
-
-export const promisify = (fn) => (...args) => 
-  new Promise((resolve, reject) => 
-    fn(...args, (err, data) => err ? reject(err) : resolve(data)))
