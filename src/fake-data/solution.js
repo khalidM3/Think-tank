d9ccf9f63211da84418985386e0e23e530d36c19
Khalid Mohamud <kmohamud1@outlook.com>
2018-07-28 21:27:42 -0700

    added structure to app

---
+++ src/fake-data/solution.js
@@ -0,0 +1,52 @@
+import faker from 'faker'
+import User from './user'
+import {looper} from './util'
+
+class Solution {
+  constructor(owner) {
+    this.id = faker.random.uuid()
+    this.owner = owner ? owner : new User()
+    this.title = faker.name.title()
+    this.content = faker.lorem.paragraph()
+    this.banner = "https://lorempixel.com/640/480/business"
+    // this.banner = 'https'+ (faker.random.image()).slice(4)
+    this.upvotes = []
+    this.downvotes = []
+  }
+}
+
+Solution.fetchFakeSolution = (cb) => {
+  try {
+  return cb(null, new Solution(new User())) 
+  } catch(err) {
+    return cb(err, null) 
+  }
+}
+
+Solution.fetchFakeSolutions = (num = 1, cb) => {
+  try {
+  let arr = []
+  looper(num)( () => {
+    return arr.push(new Solution() )
+  })
+  return cb(null, arr) 
+  } catch(err) {
+    return cb(err, null) 
+  }
+}
+
+Solution.fetchFakeUserSolutions = (num = 1, owner, cb) => {
+  try {
+    let arr = []
+    looper(num)( () => {
+      return arr.push(new Solution(owner))
+    })
+    return  cb(null, arr) 
+  } catch(err) {
+      return cb(err, null)
+  }
+}
+
+export default Solution
+
+
