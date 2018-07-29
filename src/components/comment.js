7e5a64fa5f1db41dc2c5f27ec4d484c4b24c4e89
Khalid Mohamud <kmohamud1@outlook.com>
2018-07-28 21:49:21 -0700

    cleaned up code and removed console logs and unutilizd imports

--- src/components/comment.js
+++ src/components/comment.js
@@ -1,50 +1,39 @@
 
 import React, {Component} from 'react'
 import { View, Text } from 'react-native'
-import { Card, ListItem, Button, Avatar } from 'react-native-elements'
+import { Card, Button, Avatar } from 'react-native-elements'
 
 class SolutionComment extends Component {
-
 
   render() {
     let {comment} = this.props
     return(
       <Card>
-         <View style={{
-            height: 50,
-            width: '100%',
-            flexDirection: 'row',
-            alignItems: 'center',
-            borderBottomWidth: 1,
-            borderBottomColor: 'lightgrey',
-            // justifyContent: 'space-between',
-          }}>
-              <Avatar 
-                small
-                rounded
-                activeOpacity={0.7}
-                source={{uri: comment.owner.avatar}}
-                onPress={() => this.props.onProfileInfo()}
-              />
-            <View>
-              <Text> {comment.owner.name} </Text>
-              <Text style={{color:'dimgrey'}}> {comment.owner.title} </Text>
-            </View>
-            <View>
-              {/* <Text> ... </Text> */}
-            </View>
+        <View 
+          style={{ height: 50, width: '100%', flexDirection: 'row', alignItems: 'center', borderBottomWidth: 1, borderBottomColor: 'lightgrey'}}>
+          <Avatar 
+            small
+            rounded
+            activeOpacity={0.7}
+            source={{uri: comment.owner.avatar}}
+            onPress={() => this.props.onProfileInfo()}
+          />
+          <View>
+            <Text> {comment.owner.name} </Text>
+            <Text style={{color:'dimgrey'}}> {comment.owner.title} </Text>
+          </View>
         </View>
 
         <Text style={{margin: 5}}>
           { comment.content}
         </Text>
 
-         <View
-            style={{ flex:1, flexDirection:'row',justifyContent:'flex-start',alignItems: 'center', backgroundColor:'white'}}>
-            <Button  title="upvote"color="dodgerblue" buttonStyle={{borderRadius:3, borderWidth:1, backgroundColor:'white', padding:5, borderColor:'dodgerblue'}}/>
-            <Button title="downvote" color="dodgerblue" buttonStyle={{borderRadius:3, borderWidth:1, backgroundColor:'white', padding:5, borderColor:'dodgerblue'}}/>
-            <Button title="comment" color="dodgerblue" buttonStyle={{borderRadius:3, borderWidth:1, backgroundColor:'white', padding:5, borderColor:'dodgerblue'}}/>
-          </View>
+        <View
+          style={{ flex:1, flexDirection:'row',justifyContent:'flex-start',alignItems: 'center', backgroundColor:'white'}}>
+          <Button  title="upvote"color="dodgerblue" buttonStyle={{borderRadius:3, borderWidth:1, backgroundColor:'white', padding:5, borderColor:'dodgerblue'}}/>
+          <Button title="downvote" color="dodgerblue" buttonStyle={{borderRadius:3, borderWidth:1, backgroundColor:'white', padding:5, borderColor:'dodgerblue'}}/>
+          <Button title="comment" color="dodgerblue" buttonStyle={{borderRadius:3, borderWidth:1, backgroundColor:'white', padding:5, borderColor:'dodgerblue'}}/>
+         </View>
       </Card>
     )
   }
