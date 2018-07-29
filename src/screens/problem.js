88a8f1d4c3bc61f7e1770f4127fa829910f26dd4
Khalid Mohamud <kmohamud1@outlook.com>
2018-07-28 21:12:57 -0700

    adding style and small fixes

--- src/screens/problem.js
+++ src/screens/problem.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react'
-import { ScrollView, View, Text, FlatList } from 'react-native'
-import { Card, Tile, List, ListItem } from 'react-native-elements'
+import { ScrollView, View, Text, Button } from 'react-native'
+import { Card, Tile, List, ListItem, Button as Btn } from 'react-native-elements'
 import SolutionTile from '../components/solution-tile'
+import ProblemTile from '../components/problem-tile'
 import {fetchSolutions, fetchUsers, fetchProblems} from '../config/data/index'
 
 
@@ -21,34 +22,44 @@ class Problem extends Component {
 
   onPressSolutions = () => {
     fetchSolutions(10)
-    .then( solutions => this.setState({solutions, showSolutions: true}))
+    .then( solutions => this.setState({solutions, showView: 'solutions'}))
   }
 
   onPressSubProblems = () => {
     console.log('FEtched followers ====> \n')
     fetchProblems(10)
-    .then( subProblems => this.setState({subProblems, showSubProblem: true}) )
+    .then( subProblems => this.setState({subProblems, showView: 'subproblems'}) )
   }
 
   onPressFollowers = () => {
     console.log('FEtched followers ====> \n')
     fetchUsers(10)
-    .then( followers => this.setState({followers, showFollowers: true}) )
+    .then( followers => this.setState({followers, showView: 'followers'}) )
   }
 
   onLearnMore = (problem) => {
-    this.props.navigation.navigate('Problem', { ...problem });
+    this.props.navigation.navigate('Problem', { ...problem })
   }
   onReadMore = (solution) => {
-    this.props.navigation.navigate('Solution', { ...solution });
+    this.props.navigation.navigate('Solution', { ...solution })
+  }
+  onUserDetails = (user) => {
+    this.props.navigation.navigate('Details', { ...user })
+  }
+  showSolutionForm = () => {
+    this.props.navigation.navigate('SolutionForm')
+  }
+
+  showProblemForm = () => {
+    this.props.navigation.navigate('ProblemForm')
   }
 
   render() {
     let { name, desc } = this.props.navigation.state.params
-    let { solutions, followers, subProblems } = this.state
+    let { solutions, followers, subProblems, showView } = this.state
     return (
       <ScrollView>
-        <View>
+        <View style={{backgroundColor:'white', marginTop:0}}>
           <Text 
           style={{alignSelf: 'center', padding:5, fontSize: 30, fontWeight:'bold'}}>
            {name} </Text>  
@@ -56,47 +67,44 @@ class Problem extends Component {
           style={{color: 'dimgrey', padding:10}}
           > {desc} </Text>  
         </View>
-        <View style={{ 
-          flex: 1,
-          flexDirection: 'row',
-          justifyContent: 'space-around',
-          alignItems: 'center',
-          }}>
-          <Text> Solutions </Text>  
-          <Text> Sub Problems </Text>  
-          <Text> Followers </Text>  
-        </View>
-
+        <View 
+          style={{ flex:1,flexDirection:'row',justifyContent:'space-around',alignItems: 'center', backgroundColor:'white', padding:10, borderTopWidth:1, borderColor:'lightgrey'}}>
+          <Button title="Solutions" onPress={this.onPressSolutions}/>
+          <Button title="Sub Problems" onPress={this.onPressSubProblems}/>
+          <Button title="Followers" onPress={this.onPressFollowers}/>
+          </View>
         <View>
 
+        <View style={{margin: 10}}>
+        { showView == 'solutions'  ?  <Btn title="Post Solution" backgroundColor="dodgerblue" onPress={this.showSolutionForm}/> : undefined }
+        { showView == 'subproblems'?  <Btn title="Post Sub Problem" backgroundColor="dodgerblue" onPress={this.showProblemForm}/> : undefined }
+        { showView == 'followers'?  <Btn title="Follow" backgroundColor="dodgerblue" /> : undefined }
+        </View>
 
-        { solutions.map( solution =>  
+        { showView !== 'solutions' ? undefined : solutions.map( solution =>
           <SolutionTile
             key={solution.id}
             solution={solution}
             onReadMore={() => this.onReadMore(solution)}
           /> )}
 
-        <List>
-          {subProblems.map( subProblem => (
-            <ListItem
+        { showView !== 'subproblems' ? undefined : subProblems.map( subProblem => (
+            <ProblemTile
               key={subProblem.id}
-              title={subProblem.name}
-              subtitle={subProblem.desc}
-              onPress={() => this.onLearnMore(subProblem)}
+              problem={subProblem}
+              onLearnMore={() => this.onLearnMore(subProblem)}
             />
           ))}
-        </List>
-
+        
         <List>
-        { followers.map( user => 
+        { showView !== 'followers' ? undefined : followers.map( user => 
           <ListItem
             key={user.id}
             roundAvatar
             avatar={{ uri: user.avatar }}
             title={user.name}
             // subtitle={user.email}
-            onPress={() => this.onLearnMore(user)}
+            onPress={() => this.onUserDetails(user)}
           />
         )}
         </List>
