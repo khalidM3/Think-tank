9161852d4c409b7a4f6688354920232804934426
Khalid Mohamud <kmohamud1@outlook.com>
2018-07-29 19:11:19 -0700

    small changes and fixes

--- src/components/solution-tile.js
+++ src/components/solution-tile.js
@@ -8,9 +8,13 @@ class SolutionTile extends Component {
   render() {
     let {solution} = this.props
     return(
-      <Card title={solution.title} image={{uri: solution.banner}}>
+      <Card title={
+        solution.title
+        // 'Homelessness in seattle'
+        } image={{uri: solution.banner}}>
         <Text style={{marginBottom: 10}}>
           { solution.content}
+
         </Text>
         <Button
           title='Read more'
